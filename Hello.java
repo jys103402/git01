614f7a77ccaf384d34d8aaa7ffb5ed0f3705b2eb
java <java>
2017-02-02 16:41:35 +0900

    static change

--- Hello.java
+++ Hello.java
@@ -7,9 +7,10 @@ public class Hello{
 		
 			System.out.println("Hello Git");
 		}
-		Calculator c = new Calculator();
 
-		System.out.println("3 + 4 = " + c.add(3,4));
-		System.out.println("3 - 4 = " + c.substrat(3,4));
+		System.out.println("3 + 4 = " + Calculator.add(3,4));
+		System.out.println("3 - 4 = " + Calculator.substrat(3,4));
+		System.out.println("3 * 4 = " + Calculator.multiply(3,4));
+		System.out.println("3 / 4 = " + Calculator.divide(3,4));
 	}
 }
