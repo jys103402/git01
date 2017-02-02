642b888fdd84a6a8dcce2ff25bd6dc1e3ffcf278
java <java>
2017-02-02 15:32:24 +0900

    Hello.java modify

--- Hello.java
+++ Hello.java
@@ -2,7 +2,10 @@
 public class Hello{
 
 	public static void main(String[] args){
+
+		for(int i = 0 ; i<10; i++){
 		
-		System.out.println("Hello Git");
+			System.out.println("Hello Git");
+		}
 	}
 }
